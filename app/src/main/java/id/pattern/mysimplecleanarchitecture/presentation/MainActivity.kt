fe4335b4dc5d06fac51a4d4d0a534ec9f3027fec
AnanditaRian19 <rizkianandita6@gmail.com>
2021-04-26 10:36:34 +0700

    Text error message

--- app/src/main/java/id/pattern/mysimplecleanarchitecture/presentation/MainActivity.kt
+++ app/src/main/java/id/pattern/mysimplecleanarchitecture/presentation/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         val factory = MainViewModelFactory.getInstance()
         val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
 
-        viewModel.setName("Dicoding")
+        viewModel.setName("Dico")
         viewModel.message.observe(this, {
             binding.tvWelcome.text = it.welcomeMessage
         })
